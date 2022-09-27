fc238d8add363d21144ec0f49bdee93eb69597d7
GreenDiggy <grn.riley@gmail.com>
2022-09-26 19:42:38 -0500

    backup

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
+const TEXT_STORAGE_ITEM = "text";
+const BOOL_STORAGE_ITEM = "checks";
+
 const form = document.querySelector('form');
 const list = document.querySelector('ul');
-const clear = document.getElementById('clear-button');
 const log = document.getElementById('log');
+const clear = document.getElementById('clear-button');
+const display = document.getElementById('display-btn');
 
-const data = JSON.parse(localStorage.getItem("items") || '[]');
 
-function liMaker(input) {
+const data = JSON.parse(localStorage.getItem(TEXT_STORAGE_ITEM) || '[]');
+const checks = JSON.parse(localStorage.getItem(BOOL_STORAGE_ITEM) || '[]');
+
+var inc = 0;
+
+function liMaker(input, check) {
 	var item = document.createElement('li');
-	var text = document.createElement('p');
+	var paragraph = document.createElement('p');
+	var checkbox = document.createElement('input');
 
 	item.id = "item";
-	text.innerHTML = input;
+	paragraph.innerHTML = input;
+
+	checkbox.id = "check_" + inc;
+	checkbox.type = "checkbox";
+	checkbox.checked = check;
+
+	checkbox.addEventListener('change', checkEvent);
 	
-	item.appendChild(text);
+	item.appendChild(checkbox);
+	item.appendChild(paragraph);
 	list.appendChild(item);
+
+	checks.push(check);
+
+	inc++;
 }
 
 function onPageLoad() {
 	for (var i = 0; i < data.length; i++) {
-		liMaker(data[i]);
+		liMaker(data[i], checks[i]);
 	}
 
 }
@@ -27,24 +47,39 @@ function logSubmit(event) {
 	event.preventDefault();
 
 	var input = document.getElementById('text-input').value;
+	form.reset();
 
 	log.textContent = "Form Submitted! Time stamp : " + event.timeStamp;
 
 	data.push(input);
-	localStorage.setItem("items", JSON.stringify(data));
+	saveData();
 	
-	liMaker(input);
+	liMaker(input, false);
+}
+
+function checkEvent(event) {
+
+	console.log(event.target.id);
+	saveData();
+}
+
+function saveData() {
+	localStorage.setItem(TEXT_STORAGE_ITEM, JSON.stringify(data));
+	localStorage.setItem(BOOL_STORAGE_ITEM, JSON.stringify(checks));
 }
 
 function clearData() {
+	data = [];
+	checks = [];
 	localStorage.clear();
 	list.innerHTML = "";
 }
 
 function showData() {
-	console.log(JSON.stringify(data));
+	console.log(JSON.stringify(checks));
 }
 
 document.addEventListener('DOMContentLoaded', onPageLoad);
 form.addEventListener('submit', logSubmit);
 clear.addEventListener('click', clearData);
+display.addEventListener('click', showData);
